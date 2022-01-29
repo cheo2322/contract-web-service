1acd1836acd4881ed5ec725e84658871bd3c58e2
Sergio Hidalgo-Espinoza <barcecheo2319@gmail.com>
2022-01-29 00:31:52 -0500

    implementing endpoint to get contract

--- src/main/java/com/threeastronauts/api/contract/controller/ContractController.java
+++ src/main/java/com/threeastronauts/api/contract/controller/ContractController.java
@@ -1,11 +1,14 @@
 package com.threeastronauts.api.contract.controller;
 
 import com.threeastronauts.api.contract.domain.request.ContractPostRequest;
+import com.threeastronauts.api.contract.dto.ContractDto;
 import com.threeastronauts.api.contract.service.ContractService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/contract-api")
 public class ContractController {
 
   @Autowired
@@ -23,5 +26,13 @@ public class ContractController {
   @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<Void> postNewContract(@RequestBody @Valid ContractPostRequest request) {
     return contractService.setUpNewContract(request);
+  }
+
+  @GetMapping("/{vendorId}/contracts/{contractId}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<ContractDto> getContractById(@PathVariable Long vendorId,
+      @PathVariable Long contractId) {
+
+    return ResponseEntity.ok(contractService.getContract(contractId));
   }
 }
